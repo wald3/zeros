6f8496b3d9393b40a11eac6d98ca52867b94ef3a
Rys Vladislav <onesssse@gmail.com>
2018-09-15 03:14:04 +0300

    coolest task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let result = 0;
+  let a = 5;
+  while(Math.floor(number / a) > 0){
+      result += Math.floor(number / a);
+      a *= 5;  
+  }
+  return result;
 }
